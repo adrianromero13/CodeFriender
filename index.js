a50ab3a856b0bdccecb909e127dd0d44eafcd68b
Adrian Romero <radrian031613@gmail.com>
2020-04-18 12:16:05 -0700

    cleared out unnecessary boiler content.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
 }
 
 app.use(express.json());
