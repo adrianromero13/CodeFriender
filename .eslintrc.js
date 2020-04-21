6fc9524dcaeb2de3b4e50fff0b650b313867af23
Mark Younan <markyounan@gmail.com>
2020-04-20 19:17:36 -0700

    modified .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+        // windows linebreaks when not in production environment
+        "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
+    }
 };
