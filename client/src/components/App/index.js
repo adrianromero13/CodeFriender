77a1b9d3dd6d32e7f72e8e261918df265cd259c6
armande925 <armande.milhouse@gmail.com>
2020-04-23 10:44:31 -0700

    Added header and styling cues toward a sticky footer

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,11 +2,16 @@ import React from "react";
 // import Counter from './../../components/Counter';]\
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-
+import Banner from './../Banner';
 import UserForm from './../UserForm';
+import Footer from './../Footer';
+
+
 const App = props => (
   <div className="container">
+    <Banner/>
     <UserForm/>
+    <Footer/>
   </div>
   // <Router>
   //   <div className="container">
---
+++ client/src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import 'semantic-ui-css/semantic.min.css';
+
+const Banner = () => (
+<h3 className="ui block header">
+  Code Friender
+</h3>
+
+);
+
+export default Banner;
