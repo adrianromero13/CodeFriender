829fec3c6ca5937a7acefecbfc8a20ae5abbd8e7
armande925 <armande.milhouse@gmail.com>
2020-04-26 18:33:23 -0700

    Changed footer styling and placed form in a div

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-// import 'semantic-ui-css/semantic.min.css';
-// import './../../Assets/CSS/style.css';
+// import './semantic-ui-css/semantic.min.css';
+import './../../Assets/CSS/style.css';
 
 const Footer = () => (
     // <footer className="ui block footer">nerdGang LLC 2020</footer>
-    <footer className="ui block footer fluid">
-    Code Friender
+    <footer className="ui block">
+    {/* <div className="footertext"> */}
+    <p className="footertext">Code Friender</p>
+    {/* </div> */}
     </footer>
 
 );
