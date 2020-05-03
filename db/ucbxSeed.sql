21eda91020ea2a38c428aae16e23cc5616782a4f
Adrian Romero <radrian031613@gmail.com>
2020-05-03 16:56:23 -0700

    modified seed for db

--- db/ucbxSeed.sql
+++ db/ucbxSeed.sql
@@ -1,8 +1,8 @@
 USE developer_db;
 
-INSERT INTO ucbxuser (first_name, last_name, strength, weakness, bio, email, github, badge)
+INSERT INTO ucbxusers (first_name, last_name, strength, weakness, bio, email, github, badge)
 VALUES 
 ('Adrian', 'Romero', 'React', 'Javascript', 'Take me home', 'adrianromero0312@gmail.com', 'adrianromero13', 'https://avatars0.githubusercontent.com/u/25410826?v=4'),
 ('Mark', 'Mesina', 'Express', 'React', 'Cash me ousside how bout dah', 'markmesina69@gmail.com', 'markmesina', 'https://avatars1.githubusercontent.com/u/58279053?v=4'),
 ('Armande',	'Milhouse',	'HTML-GIT-CSS', 'React', 'Black Adonis', 'armande.milhouse@gmail.com',	'armande925',	'https://avatars0.githubusercontent.com/u/58568984?v=4'),
-('Mark', 'Younan', 'CSS-Bootstrap',	'State', 'Maker and Breaker of all that is Code', 'markyounan@gmail.com', 'markyounan11', 'https://avatars0.githubusercontent.com/u/58568984?v=4')
+('Mark', 'Younan', 'CSS-Bootstrap',	'State', 'Maker and Breaker of all that is Code', 'markyounan@gmail.com', 'markyounan11', 'https://avatars0.githubusercontent.com/u/58568984?v=4');
