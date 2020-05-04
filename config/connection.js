04fa9f5e5781a41089596c171b5e05603f6b1a72
Adrian Romero <radrian031613@gmail.com>
2020-05-04 14:03:34 -0700

    connected live database. added seed to database. Deleted unused files and code.

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'i2cpbxbi4neiupid.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
+    user: 'e7o6s1uexsuna047',
     password: process.env.SQL_PASS,
-    database: 'developer_db',
+    database: 'eyjq5knu404g2oqf',
   });
 }
 module.exports = connection;
