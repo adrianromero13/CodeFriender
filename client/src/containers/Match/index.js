6cd0ceb1404aff659ebe259118580496ef8c39be
Adrian Romero <radrian031613@gmail.com>
2020-05-03 17:09:05 -0700

    moved files from components to container folder. Removed unused code

--- client/src/containers/Match/index.js
+++ client/src/containers/Match/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Form, Input, TextArea, Button, Select, Container, Grid } from 'semantic-ui-react';
 
-import UserProfile from '../UserProfile';
+import UserProfile from '../../components/UserProfile';
 
 
 class Match extends Component {
@@ -93,83 +93,3 @@ class Match extends Component {
 }
 
 export default Match;
-
-        // <div>
-        //     <Form.Field
-        //         id='form-input-control-error-github'
-        //         control={Input}
-        //         label='Github Username'
-        //         placeholder='janey123'
-        //         onChange={this.handleInputChange}
-        //         value={this.state.github}
-        //         name='github'
-        //     />
-        //     <Form.Field
-        //         id='form-button-control-public'
-        //         control={Button}
-        //         content='Submit'
-        //         label='Click to Submit'
-        //         onClick={this.handleSubmit}
-        //     />
-
-        // </div>
-
-// currentUser: {
-//     name: "",
-//     github: "",
-//     strength: "HTML"
-// }
-
-// allUsers: [
-//     {
-//         name: "",
-//         github: "",
-//         weakness: "HTML"
-//     },
-//     {
-//         name: "",
-//         github: "",
-//     }
-// ]
-
-
-// create new Profile container
-// inside Profile
-//     render 
-//         BestMatches - 2pts - users where strength and weakness match
-//             In CDM - axios call to get users with matching strength + weakness
-//             save to state
-//             render to page
-//         BestMatchesForThem - 1pt - users where strength matches to weakness 
-//             In CDM - axios call to get users with matching strength + weakness
-//             save to state
-//             render to page
-//         BestMatchesForMe - 1pt - users where weakness matches to strength
-//             In CDM - axios call to get users with matching strength + weakness
-//             save to state
-//             render to page
-
-//mysql way
-// select * from ucbxusers where strength = current weakness
-//
-
-// Mark
-// S: HTML
-// W: CSS
-
-// Musa
-// S: JS
-// W: HTML
-
-// Adrian:
-// S: CSS
-// W: JS
-
-// Armande
-// S: CSS
-// W: HTML
-
-// CurrentUser: Mark
-// BestMatch - Mark and Armande are a 2 point match
-// BestMatchForThem - Mark and Musa are a 1 point match because not both cases are satisfied
-// BestMatchForMe - Mark and Adrian are a 1 point match because not both cases are satisfied
